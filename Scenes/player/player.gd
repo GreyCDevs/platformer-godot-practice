13b1c25fecbe209a6262cb60f265ec61b3c6ab7c
GreyCDevs <raulpavonfp@gmail.com>
2025-09-05 13:00:57 +0200

    Add animated sprite

--- Scenes/player/player.gd
+++ Scenes/player/player.gd
@@ -6,13 +6,17 @@ const ACCELERATION = 800.0
 const FRICTION = 1000.0
 const JUMP_VELOCITY = -300.0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
-	apply_gravity(delta)
+	var input_axis := Input.get_axis("ui_left", "ui_right")
 	
+	apply_gravity(delta)
 	handle_jump()
-
-	handle_movement(delta)
+	handle_movement(input_axis, delta)
+	update_animations(input_axis)
+	move_and_slide()
 
 func apply_gravity(delta: float) -> void:
 	if not is_on_floor():
@@ -27,13 +31,9 @@ func handle_jump() -> void:
 			velocity.y = JUMP_VELOCITY
 
 
-func handle_movement(delta: float) -> void:
-	var input_axis := Input.get_axis("ui_left", "ui_right")
-
+func handle_movement(input_axis: float, delta: float) -> void:
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
-
-	move_and_slide()
 
 func apply_friction(input_axis: float, delta: float) -> void:
 	if input_axis != 0: return
@@ -42,3 +42,16 @@ func apply_friction(input_axis: float, delta: float) -> void:
 func handle_acceleration(input_axis: float, delta: float) -> void:
 	if input_axis == 0: return
 	velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
+
+func update_animations(input_axis: float) -> void:
+	if input_axis != 0:
+		handle_character_orientation(input_axis)
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")		
+
+func handle_character_orientation (input_axis: float) -> void:
+	animated_sprite_2d.flip_h = input_axis < 0
