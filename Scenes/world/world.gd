44c6c2a97bff260757cafd6cbc9b9c1986a642ff
GreyCDevs <raulpavonfp@gmail.com>
2025-09-06 14:00:29 +0200

    add level completed scene

--- Scenes/world/world.gd
+++ Scenes/world/world.gd
@@ -3,7 +3,13 @@ extends Node2D
 
 @onready var collision_polygon_2d: CollisionPolygon2D = $Escenario/CollisionPolygon2D
 @onready var polygon_2d: Polygon2D = $Escenario/CollisionPolygon2D/Polygon2D
+@onready var level_completed: ColorRect = $CanvasLayer/LevelCompleted
 
 func _ready() -> void:
 	polygon_2d.polygon = collision_polygon_2d.polygon
 	RenderingServer.set_default_clear_color("black")
+	Events.level_completed.connect(show_level_completed)
+	
+func show_level_completed() -> void:
+	level_completed.show()
+	get_tree().paused = true
