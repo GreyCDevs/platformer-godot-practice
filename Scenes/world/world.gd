98cdb3ff525be6ac1042a49139fc7f130dd7464b
GreyCDevs <raulpavonfp@gmail.com>
2025-09-06 17:56:33 +0200

    Add level changed transition

--- Scenes/world/world.gd
+++ Scenes/world/world.gd
@@ -9,8 +9,10 @@ func _ready() -> void:
 	Events.level_completed.connect(show_level_completed)
 	
 func show_level_completed() -> void:
-	if next_level is PackedScene: 
-		get_tree().change_scene_to_packed(next_level)
-		return
-	get_tree().paused = true
 	level_completed.show()
+	get_tree().paused = true
+	if not next_level is PackedScene: return 
+	await LevelTransition.fade_to_black()
+	get_tree().paused = false
+	get_tree().change_scene_to_packed(next_level)
+	LevelTransition.fade_from_black()
